089d730bc61deb08e87293db1f9b0a929d1a07f7
kennykiller <vadim2505@mail.ru>
2022-02-27 22:01:27 +0300

    #14

---
+++ front/src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full grid grid-cols-7 gap-4">
+        <NavigationComponent class="col-span-1 bg-white flex justify-start p-3"></NavigationComponent>
+        <main class="col-span-6 bg-black">
+            ALE
+        </main>
+    </div>
+</template>
+
+<script setup>
+import NavigationComponent from '@/components/NavigationComponent.vue';
+</script>
