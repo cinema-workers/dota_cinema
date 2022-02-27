18ad473d5813bdc04328f2ca840aa40763e3713c
valerakovalenko <kotvalerakot@mail.ru>
2022-02-27 21:55:14 +0300

    great footer

--- front/src/components/FooterComponent.vue
+++ front/src/components/FooterComponent.vue
@@ -1,12 +1,28 @@
 <template>
-    <div class="footer">
-        Footer
-    </div>
+    <nav>
+      <ul>
+        <li><p>Dota Cinema</p></li>
+        <li class="second"><p>Санкт-Петербург</p></li>
+        <li><img src="../assets/svg/insta.svg"></li>
+        <li><img src="../assets/svg/vk.svg"></li>
+        <li><img src="../assets/svg/tg.svg"></li>
+        <li></li>
+      </ul>
+    </nav>
 </template>
 
 <style lang="scss" scoped>
-    .footer {
-        height: 45px;
-        width: 100%;
+    img {
+      height: 1.5rem;
+      width: 1.5rem;
+    }
+    nav {
+      width: 100%;
+    }
+    ul {
+      display: flex;
+    }
+    .second {
+      margin: 0 auto;
     }
 </style>
--- front/src/components/HeaderComponent.vue
+++ front/src/components/HeaderComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full py-2 extra">
+  <div class="w-full py-2">
     <nav class="w-full p-4">
         <h1>Hello World</h1>
       <ul class="flex flex-row w-full items-center justify-between">
