ea63a64a078c2d22af31dd4f5c62dee1dbb1ddc4
kennykiller <vadim2505@mail.ru>
2022-02-28 23:29:11 +0300

    #14 сайдбар, мэйн

---
+++ front/src/components/carousel/CarouselItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div><img src="../../../src/assets/posters/p4653.jpg" alt="" /></div>
+        <div>
+            <h3>{{ props.name }}</h3>
+            <p>{{ props.types }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+interface Props {
+    url: string,
+    types: string[],
+    name: string,
+}
+const props = defineProps<Props>();
+</script>
--- front/src/views/HomePage.vue
+++ front/src/views/HomePage.vue
@@ -2,11 +2,31 @@
     <div class="w-full grid grid-cols-7 gap-4">
         <NavigationComponent class="col-span-1 bg-white flex justify-start p-3"></NavigationComponent>
         <main class="col-span-6 bg-black">
-            ALE
+            <section class="flex justify-between">
+                <CarouselItem v-for="poster in posters" :key="JSON.stringify(poster)" :url="poster.url" :name="poster.name" :types="poster.types">
+                </CarouselItem>
+            </section>
         </main>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import NavigationComponent from '@/components/NavigationComponent.vue';
+import CarouselItem from '@/components/carousel/CarouselItem.vue';
+
+interface poster {
+    url: string,
+    name: string,
+    types: string[]
+}
+
+enum filmType { Thriller = "триллер", Comedy = "комедия", Family = "семейный", Sport = "спорт", 
+    Fighter = "боевик", Tragedy = "драма", Adventure = "приключения", Cartoon = "мультфильм" }
+
+const posters: poster[] = [{ url: '@/src/assets/posters/p4653.jpg', name: 'Анчартед', types: [filmType.Fighter, filmType.Adventure] },
+{ url: '@/src/assets/posters/krol.jpg', name: 'Кролецып и хомяк тьмы', types: [filmType.Cartoon] },
+{ url: '@/src/assets/posters/gg.jpg', name: 'Мистер Нокаут', types: [filmType.Tragedy, filmType.Sport] },
+{ url: '@/src/assets/posters/nil.jpg', name: 'Смерть на Ниле', types: [filmType.Thriller] },
+{ url: '@/src/assets/posters/lulu.jpg', name: 'Лулу и Бриггс', types: [filmType.Comedy, filmType.Adventure, filmType.Cartoon] },]
+
 </script>
