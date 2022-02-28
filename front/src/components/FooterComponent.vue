795da4193a1445108404833143669e5e6f8ed79f
valerakovalenko <kotvalerakot@mail.ru>
2022-02-28 22:47:12 +0300

    #13 final footer

--- front/src/components/FooterComponent.vue
+++ front/src/components/FooterComponent.vue
@@ -1,11 +1,31 @@
 <template>
-    <nav class="w-full">
+    <nav class="footer__nav w-full p-4">
       <ul class="flex">
-        <li><p>Dota Cinema</p></li>
-        <li class="mx-auto"><p>Санкт-Петербург</p></li>
-        <li><img src="../assets/svg/insta.svg"></li>
-        <li><img src="../assets/svg/vk.svg"></li>
-        <li><img src="../assets/svg/tg.svg"></li>
+        <li>
+          <router-link to="/">
+            <p>Dota Cinema</p>
+          </router-link>
+        </li>
+        <li class="mx-auto">
+          <router-link to="/">
+            <p>Санкт-Петербург</p>
+          </router-link>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/government_rf/">
+            <img src="../assets/svg/insta.svg">
+          </a>
+        </li>
+        <li>
+          <a href="https://vk.com/rogachev7">
+            <img src="../assets/svg/vk.svg">
+          </a>
+        </li>
+        <li>
+          <a href="https://t.me/zeykanm">
+            <img src="../assets/svg/tg.svg">
+          </a>
+        </li>
         <li></li>
       </ul>
     </nav>
